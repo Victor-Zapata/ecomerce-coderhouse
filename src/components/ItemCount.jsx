b8668156fbd0bb537529715808c310d267427b0e
pengyi-labs <lastresmoyallc@gmail.com>
2022-04-03 22:32:25 -0300

    images2

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 
 const ItemCount = ({ stock, initial, setInitial, onAdd }) => {
-    const increment = () => {
-        setInitial(initial+1)
-    }
+  const increment = () => {
+    setInitial(initial + 1);
+  };
 
-    const decrement = () => {
-        if(initial == 0){
-            return
-        }
-        setInitial(initial-1)
+  const decrement = () => {
+    if (initial == 0) {
+      return;
     }
+    setInitial(initial - 1);
+  };
 
   return (
     <div>
       <div className="d-flex">
-        <button type="" onClick={decrement}>-</button>
-        <input type="text" value={initial} />
+        <button className="btn btn-danger" type="" onClick={decrement}>
+          -
+        </button>
+        <input type="text" className="input-group-text text-center" value={initial} />
 
-        <button type="" onClick={increment}>+</button>
+        <button className="btn btn-success" type="" onClick={increment}>
+          +
+        </button>
       </div>
-
-      <button type="" onClick={onAdd} disabled={initial > stock ? true : false} >Agregar al Carrito</button>
+      <div className="py-2">
+        <button
+          className="btn btn-primary"
+          type=""
+          onClick={onAdd}
+          disabled={initial > stock ? true : false}
+        >
+          Agregar al Carrito
+        </button>
+      </div>
     </div>
   );
 };
